0d8630ef6339a37446ecaec7007281c6cb078db4
Giustino Yu <giustinoyu@MacBook-Pro.local>
2017-01-08 15:33:34 +0800

    add permission check on show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -9,7 +10,6 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    find_group_and_check_permission
   end
 
   def new
@@ -28,10 +28,6 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:id])
-
-    find_group_and_check_permission
-
     if @group.update(group_params)
        redirect_to groups_path, notice: "Update Success"
     else
@@ -40,13 +36,8 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
-
-  find_group_and_check_permission
-
     @group.destroy
-    flash[:alert] = "Group deleted"
-    redirect_to groups_path
+    redirect_to groups_path,flash[:alert] = "Group deleted"
   end
 
   private
